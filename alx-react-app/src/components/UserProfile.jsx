7ab7b4d2e7c58bad09884c350222177fb89a32ad
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-02-14 03:41:23 -0800

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,1 +1,12 @@
+const UserProfile = (props) => {
+    return (
+        <div style={{ border: "1px solid #ddd", padding: "20px", borderRadius: "10px", maxWidth: "300px", textAlign: "center", margin: "20px auto" }}>
+            <h2>{props.name}</h2>
+            <p><strong>Age:</strong> {props.age}</p>
+            <p><strong>Bio:</strong> {props.bio}</p>
+        </div>
+    );
+};
+
+export default UserProfile;
 
