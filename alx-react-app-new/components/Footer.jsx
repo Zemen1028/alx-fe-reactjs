b8d65c49c081e114cb68947b4c5b1899679ab7ee
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-02-23 00:46:39 -0800

    Update Footer.jsx

--- alx-react-app-new/components/Footer.jsx
+++ alx-react-app-new/components/Footer.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', position: 'fixed', bottom: '0', width: '100%' }}>
+      <p>&copy; 2025 My Favorite Cities</p>
+    </footer>
+  );
+};
+
+export default Footer;
 
