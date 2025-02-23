9e5831337c9e277927b977770459be5e318629cd
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-02-23 01:40:06 -0800

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,4 +1,11 @@
 import ProfilePage from './ProfilePage';
+// src/UserContext.js
+import React from 'react';
+
+// Create a Context
+const UserContext = React.createContext();
+
+export default UserContext;
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
