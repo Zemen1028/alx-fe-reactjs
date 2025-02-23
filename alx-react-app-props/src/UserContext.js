b2ba73d00767c348b7e56c343a98c6d8e487b390
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-02-23 04:21:11 -0800

    Delete alx-react-app-props directory

--- alx-react-app-props/src/UserContext.js
+++
@@ -1,5 +0,0 @@
-// src/UserContext.js
-import React from 'react';
-// Create a Context
-const UserContext = React.createContext();
-export default UserContext;
--- alx-react-app-props/src/components/ProfilePage.jsx
+++
@@ -1,9 +0,0 @@
-// ProfilePage.jsx
-import React from 'react';
-import UserInfo from './UserInfo';
-
-function ProfilePage() {
-  return <UserInfo />;
-}
-
-export default ProfilePage;
