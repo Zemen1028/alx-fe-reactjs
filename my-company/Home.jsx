87b4b77ef7065b681ed2d4a30ae8422efde689fa
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-02-23 03:38:31 -0800

    Update Home.jsx

--- my-company/Home.jsx
+++ my-company/Home.jsx
@@ -1,10 +1,15 @@
- function Home() {
-     return (
-       <div style={{ padding: '20px' }}>
-         <h1>Welcome to Our Company</h1>
-         <p>We are dedicated to delivering excellence in all our services.</p>
-       </div>
-     );
-   }
+// src/components/Home.jsx
+import React from 'react';
 
-   export default Home;
+const Home = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ color: 'navy' }}>Welcome to Our Company</h1>
+      <p style={{ fontSize: '18px', color: 'gray' }}>
+        We are dedicated to delivering excellence in all our services.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
