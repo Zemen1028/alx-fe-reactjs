32135522d7e73352f89045e1067711d21053b131
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-02-23 01:34:31 -0800

    Update UserDetails.jsx

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,1 +1,17 @@
+// src/UserDetails.jsx
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
+function UserDetails() {
+  // Consume the context
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
