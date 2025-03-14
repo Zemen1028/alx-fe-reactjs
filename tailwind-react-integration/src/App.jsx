9e9e96bc544b7f4a5cface2e6c9f3403b67b8415
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-14 05:39:10 -0700

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <div>
+      <UserProfile />
+    </div>
 
 <div className="bg-gray-100 min-h-screen flex items-center justify-center">
       <h1 className="text-3xl font-bold text-blue-600">
