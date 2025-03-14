ecde2837b59babb3eb52e9ee15472b66a9d33fdd
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-14 05:29:24 -0700

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'media', // or 'class'
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
