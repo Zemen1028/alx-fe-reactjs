02744693900b5dc6514866287342fee36409b8fd
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-20 12:54:16 -0700

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -5,13 +5,21 @@ import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+<Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
          <div className="min-h-screen bg-gray-100">
       <HomePage />
     </div>
