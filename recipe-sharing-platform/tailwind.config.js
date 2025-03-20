40547d2d7e824d9979937575da690ec7480af18b
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-20 04:12:56 -0700

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", // This is the correct path for Vite projects
+    "./index.html",
+    "./public/index.html",// This is the correct path for Vite projects
     "./src/**/*.{js,jsx,ts,tsx}", // This scans all JS/TS/JSX/TSX files in the src folder
   ],
   theme: {
