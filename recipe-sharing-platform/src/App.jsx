96919772a9feefefb629d924e411e367c182a3a9
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-20 04:16:21 -0700

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -4,12 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomePage from "./components/HomePage";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+         <div className="min-h-screen bg-gray-100">
+      <HomePage />
+    </div>
       <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow p-4">
