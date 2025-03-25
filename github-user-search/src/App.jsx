fc14288d1b1cc4d5a3033dfa4b7a3bb5bb3b3bd8
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-25 01:43:40 -0700

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -10,6 +10,10 @@ function App() {
 
   return (
     <>
+          <div className="App">
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
         <div className="app">
       <h1>GitHub User Search</h1>
       {/* Search component will go here */}
