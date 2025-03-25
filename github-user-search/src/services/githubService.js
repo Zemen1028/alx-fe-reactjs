39735edf7eda920f2a6319bbdb0a0bb340c3fa91
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-25 01:41:21 -0700

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
 
+const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      throw new Error('User not found');
+    }
+    throw error;
+  }
+};
+
+export { fetchUserData };
