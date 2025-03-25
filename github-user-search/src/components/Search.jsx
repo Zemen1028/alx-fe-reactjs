ac692b5e1d665c9fb01fc463506cc0898f52a39c
Zemen Debebe <134845404+Zemen1028@users.noreply.github.com>
2025-03-25 01:53:40 -0700

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -11,7 +11,11 @@ const Search = () => {
     setLoading(true);
     setError("");
     setUser(null);
-
+    const result = await onSearch(query); // Assuming onSearch returns a result or null
+  if (!result) {
+      setError("Looks like we can't find the user");
+    }
+  };
     try {
       const response = await fetch(`https://api.github.com/users/${query}`);
       if (!response.ok) {
